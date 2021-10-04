42ef8259e8d4a4d9dfd576a5906c5440d557338e
Pai Peng <p.peng@s2icode.com>
2021-10-04 15:48:18 +0800

    validate token via db

--- src/main/java/com/paipeng/idcard/repository/UserRepository.java
+++ src/main/java/com/paipeng/idcard/repository/UserRepository.java
@@ -4,9 +4,14 @@ import com.paipeng.idcard.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
 
     User findByEmailAndPassword(String email, String password);
+
+    @Query("SELECT u FROM User u WHERE u.token is not null")
+    List<User> findUsersWithToken();
 }
--- src/main/java/com/paipeng/idcard/security/JWTAuthorizationFilter.java
+++ src/main/java/com/paipeng/idcard/security/JWTAuthorizationFilter.java
@@ -1,9 +1,12 @@
 package com.paipeng.idcard.security;
 
 
+import com.paipeng.idcard.entity.User;
+import com.paipeng.idcard.repository.UserRepository;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +24,11 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
     private final static Logger logger = LoggerFactory.getLogger(JWTAuthorizationFilter.class);
     private final String HEADER = "Authorization";
     private final String PREFIX = "Bearer ";
-    public static final String SECRET = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoidGVzdEBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjMzMzIxODk5LCJleHAiOjE2MzMzMjI0OTl9.8R8yST-AL58Cb7m5GXIGMsaAhsVW_Nar-gcLJJc1AKueeqtfVuDSQKJuO5_IYYkg2-pGSBL7BYJbY6QYv-uR5w";
+    //public static final String SECRET = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dGVrSldUIiwic3ViIjoidGVzdEBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjMzMzIxODk5LCJleHAiOjE2MzMzMjI0OTl9.8R8yST-AL58Cb7m5GXIGMsaAhsVW_Nar-gcLJJc1AKueeqtfVuDSQKJuO5_IYYkg2-pGSBL7BYJbY6QYv-uR5w";
+
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
@@ -50,11 +57,16 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         logger.trace("validateToken");
         String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
         logger.trace("jwtToken: " + jwtToken);
-        logger.trace("local SECRET: " + SECRET);
-        if (SECRET.equals(jwtToken)) {
-            logger.trace("token string valid");
+
+        List<User> users = userRepository.findUsersWithToken();
+        for (User user : users) {
+            logger.trace("local SECRET: " + user.getToken());
+            Claims claims = Jwts.parser().setSigningKey(user.getToken().getBytes()).parseClaimsJws(jwtToken).getBody();
+            if (claims != null) {
+                return claims;
+            }
         }
-        return Jwts.parser().setSigningKey(SECRET.getBytes()).parseClaimsJws(jwtToken).getBody();
+        return null;
     }
 
     /**
