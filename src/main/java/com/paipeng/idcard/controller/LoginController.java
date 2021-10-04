e406635d6e2bd0111b1a333b87192161b489f9e2
Pai Peng <p.peng@s2icode.com>
2021-10-04 17:12:24 +0800

    logout return 204

--- src/main/java/com/paipeng/idcard/controller/LoginController.java
+++ src/main/java/com/paipeng/idcard/controller/LoginController.java
@@ -6,7 +6,11 @@ import com.sun.istack.NotNull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class LoginController {
@@ -24,10 +28,13 @@ public class LoginController {
         }
     }
 
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @GetMapping("/logout2")
-    public String logout() {
+    public void logout() {
         logger.info("logout2");
         userService.logout();
-        return "";
+        //response.setStatus(HttpStatus.NO_CONTENT.value());
+        //return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
