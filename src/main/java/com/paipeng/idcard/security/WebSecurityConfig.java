b5604f6931f684bc37922dbe1b009ec6ec6eaf7f
Pai Peng <p.peng@s2icode.com>
2021-10-04 21:03:52 +0800

    reformat code

--- src/main/java/com/paipeng/idcard/security/WebSecurityConfig.java
+++ src/main/java/com/paipeng/idcard/security/WebSecurityConfig.java
@@ -1,11 +1,8 @@
 package com.paipeng.idcard.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
