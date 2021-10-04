2e8d018efac84bb60fa19630c6738bb280827f18
Pai Peng <p.peng@s2icode.com>
2021-10-04 10:46:35 +0800

    add log and bind database to security login

--- src/main/java/com/paipeng/idcard/controller/VersionController.java
+++ src/main/java/com/paipeng/idcard/controller/VersionController.java
@@ -1,13 +1,40 @@
 package com.paipeng.idcard.controller;
 
+import com.paipeng.idcard.entity.User;
+import com.paipeng.idcard.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.List;
 
 @RestController
 public class VersionController {
+    private Logger log;
+    @Autowired
+    private UserRepository userRepository;
+
+    public VersionController() {
+        this.log = LogManager.getLogger(this.getClass().getName());
+    }
+
     @GetMapping("/version")
     public String version() {
+        log.trace("version");
         return "Hello world";
+    }
+
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepository.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
     }
 
 }
---
+++ src/main/java/com/paipeng/idcard/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.paipeng.idcard.security;
+
+import com.paipeng.idcard.entity.User;
+import com.paipeng.idcard.repository.UserRepository;
+import com.paipeng.idcard.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
