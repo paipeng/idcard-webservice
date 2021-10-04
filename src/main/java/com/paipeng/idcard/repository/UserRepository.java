3f716bf8d76fd26988ed129e3fc715e0f813f4e5
Pai Peng <p.peng@s2icode.com>
2021-10-04 10:09:30 +0800

    add repository and unit test

---
+++ src/main/java/com/paipeng/idcard/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.paipeng.idcard.repository;
+
+import com.paipeng.idcard.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+
+}
---
+++ src/test/java/com/paipeng/idcard/repository/UserRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.paipeng.idcard.repository;
+
+import com.paipeng.idcard.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("test@m.c");
+        user.setPassword("123456");
+        user.setFirstName("Test");
+        user.setLastName("Home");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+        assertEquals(user.getEmail(), existUser.getEmail());
+    }
+    @Test
+    void findByEmail() {
+    }
+}
